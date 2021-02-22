37ee0564ddc91aac63297416103bd8d22c1bd22a
Nelson Foltz <nelsfoltz@gmail.com>
2021-02-22 09:32:31 -0500

    add: styled components for header elements

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "2b81e68aa097a00aeacd";
+/******/ 	var hotCurrentHash = "144901ed499476b08497";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
