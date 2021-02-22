41d026ed4515178e38b47d6b1c67b944fdd99024
Nelson Foltz <nelsfoltz@gmail.com>
2021-02-22 10:35:50 -0500

    visualizing route changes

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "73750a108576d9555a69";
+/******/ 	var hotCurrentHash = "3237c12f602f81a79224";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
