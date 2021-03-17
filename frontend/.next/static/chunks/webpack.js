f6ace7deda738893e25a207d69bde9b1339ea632
Nelson Foltz <nelsfoltz@gmail.com>
2021-03-17 10:57:52 -0400

    26: displaying single items, routing, and SEO

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "7df2026d8ec4fb7acf3b";
+/******/ 	var hotCurrentHash = "6151a273d4cd759f7799";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
