558e198f8cfd6f732304252d8324ca3769f07ef6
Nelson Foltz <nelsfoltz@gmail.com>
2021-03-22 13:41:54 -0400

    cart and order based rules

--- backend/schemas/CartItem.ts
+++ backend/schemas/CartItem.ts
@@ -1,14 +1,21 @@
 import { integer, select, text, relationship } from '@keystone-next/fields';
 import { list } from '@keystone-next/keystone/schema';
+import { rules, isSignedIn } from '../access';
 
 export const CartItem = list({
+  access: {
+    create: isSignedIn,
+    read: rules.canOrder,
+    update: rules.canOrder,
+    delete: rules.canOrder,
+  },
   ui: {
     listView: {
       initialColumns: ['product', 'quantity', 'user'],
     },
   },
   fields: {
-    // TODO: custom label in here
+    // TODO: Custom Label in here
     quantity: integer({
       defaultValue: 1,
       isRequired: true,
